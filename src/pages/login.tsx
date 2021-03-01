2744cabeba28fe035622b8d932a5598a5b0d5522
WRHR <wrhreeves@gmail.com>
2021-02-28 22:26:00 -0700

    Add login hook to login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,19 +4,41 @@ import { Wrapper } from "../components/Wrapper";
 import { Form, Formik } from "formik";
 import { InputField } from "../components/InputField";
 import { Box, Button, Heading } from "@chakra-ui/react";
+import { MeDocument, useLoginMutation } from "../generated/graphql";
 interface loginProps {}
-// apollo/gql mutations 
+// apollo/gql mutations
 // submit form
 // logo
 export const login: React.FC<loginProps> = ({}) => {
-  // const router = useRouter();
+  const router = useRouter();
+  const [login] = useLoginMutation();
   return (
     <Wrapper variant="small">
       <Heading>Welcome Budgeteer!</Heading>
       <Formik
         initialValues={{ usernameOrEmail: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          await console.log(values);
+          const response = await login({
+            variables: values,
+            update: (cache, { data }) => {
+              cache.writeQuery<MeQuery>({
+                query: MeDocument,
+                data: {
+                  __typename: "Query",
+                  me: data?.login.user,
+                },
+              });
+            },
+          });
+          if (response.data?.login.errors) {
+            // set errors
+          } else if (response.data?.login.user) {
+            if (typeof router.query.next === "string") {
+              router.push(router.query.next);
+            } else {
+              router.push("/");
+            }
+          }
         }}
       >
         {({ isSubmitting }) => (
